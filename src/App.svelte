010bc7de3b7c4773d214f09866320802bdd7d17c
Hollister009 <reactive.andrew@yahoo.com.ua>
2022-07-08 15:44:22 +0300

    Small refactor: Grid component

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,12 @@
 <script>
-  // const tableExample = [
-  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
-  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
-  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
-  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
-  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
-  // ];
-
+  import Grid from './lib/Grid.svelte';
   let table = [new Array(5).fill(null).map((_, index) => ++index)];
 </script>
 
 <main>
   <div class="container">
     <h1>Techboard:</h1>
-    <div class="grid">
-      {#each table as row}
-        <div class="row">
-          {#each row as cell}
-            <div class="cell">
-              <span class="cell-content">{cell}</span>
-            </div>
-          {/each}
-        </div>
-      {/each}
-    </div>
+    <Grid {table} />
   </div>
 </main>
 
@@ -40,33 +23,5 @@
   .container {
     max-width: 1080px;
     margin: 0 auto;
-  }
-
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: auto;
-    gap: 10px;
-    grid-template-areas: 'r r r r r';
-  }
-
-  .row {
-    grid-area: r;
-    display: flex;
-    gap: 10px;
-  }
-
-  .cell {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    background-color: crimson;
-    min-height: 200px;
-    flex-basis: 100%;
-  }
-
-  .cell-content {
-    color: white;
-    font-size: 80px;
   }
 </style>
---
+++ src/lib/Grid.svelte
@@ -0,0 +1,52 @@
+<script>
+  // [
+  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
+  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
+  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
+  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
+  //   ['Beginer', 'Competent', 'Advanced', 'Expert', 'Master'],
+  // ];
+  export let table = [];
+</script>
+
+<div class="grid">
+  {#each table as row}
+    <div class="row">
+      {#each row as cell}
+        <div class="cell">
+          <span class="cell-content">{cell}</span>
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: auto;
+    gap: 10px;
+    grid-template-areas: 'r r r r r';
+  }
+
+  .row {
+    grid-area: r;
+    display: flex;
+    gap: 10px;
+  }
+
+  .cell {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background-color: crimson;
+    min-height: 200px;
+    flex-basis: 100%;
+  }
+
+  .cell-content {
+    color: white;
+    font-size: 80px;
+  }
+</style>
